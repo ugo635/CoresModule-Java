f52eb7c8eafb6b4c541fb4a742269bf6105c5cdb
ugo635 <ugolechien58901@gmail.com>
2025-10-03 20:05:21 +0200

    Saving

--- src/main/java/com/me/coresmodule/CoresModule.java
+++ src/main/java/com/me/coresmodule/CoresModule.java
@@ -1,11 +1,18 @@
 package com.me.coresmodule;
 
-import net.fabricmc.api.ModInitializer;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class CoresModule implements ModInitializer {
 	public static final String MOD_ID = "coresmodule";
@@ -22,9 +29,57 @@ public class CoresModule implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		register();
 	}
 
-	public void firstCommand() {
-		
+
+
+	public static void createCommand(String name, CommandContext<ServerCommandSource> context) {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal(name)
+					.executes(CoresModule::context)
+			);
+		});
+	}
+
+	public static void register() {
+		/*
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("mymf")
+
+					// Branch with 2 argument
+					.then(CommandManager.argument("mf", StringArgumentType.string()))
+					.then(CommandManager.argument("kc", StringArgumentType.string())
+							.executes(context -> {
+								String mf = StringArgumentType.getString(context, "mf"); // Get the value of the arg 'mf'
+								String kc = StringArgumentType.getString(context, "mf"); // Get the value of the arg 'kc'
+
+								context.getSource().sendFeedback(() -> Text.literal("You put §5" + mf + "§r as an argument and " + kc), false);
+								LOGGER.info("Magic Find: " + mf + "\nKill Combo Percentage:§5 " + kc);
+								return 1;
+							})
+					)
+
+					// Branch with 1 argument
+					.then(CommandManager.argument("mf", StringArgumentType.string())
+							.executes(context -> {
+								String mf = StringArgumentType.getString(context, "mf"); // use argument name "mf"
+								context.getSource().sendFeedback(() -> Text.literal("You put " + mf + " as an argument"), false);
+								LOGGER.info("Magic Find: " + mf);
+								return 1;
+							})
+					)
+
+
+					// Branch without argument
+					.executes(context -> {
+						context.getSource().sendFeedback(() -> Text.literal("You need to put an argument"), false);
+						return 1;
+					})
+			);
+		});
+		*/
+
+
 	}
 }
