58442266a8bd58525db3a39dbc1da54c4d83c71a
ugo635 <ugolechien58901@gmail.com>
2025-12-14 15:59:29 +0100

    Fire Freeze Timer - Part 1

--- src/main/java/com/me/coresmodule/features/Diana/DianaFeatures.java
+++ src/main/java/com/me/coresmodule/features/Diana/DianaFeatures.java
@@ -21,22 +21,23 @@ public class DianaFeatures {
         Overlay overlay = new Overlay("§dFire Freeze Timer", 10.0f, 10.0f, 2.0f, List.of("Chat screen", "Crafting"));
         OverlayTextLine overlayText = new OverlayTextLine("");
         overlay.register();
-        overlay.setCondition(() -> Diana.ffTimer.get() && ffTimerOn);
+        overlay.setCondition(() -> Diana.ffTimer.get() && ffTimerOn && endTime - startTime >= 0);
         overlay.addLine(overlayText);
 
         Register.command("kingWarning", args -> {
            Helper.sleep(200, () -> {
-               Chat.chat("⚠ Do NOT use Ranged/Magic damage on Kings // No Flaming Flay / No Term / No Crimson Stacks (Dominus) // Switch to Sorrow/Mythos armor and do the hit phase with melee only ⚠");
+               Chat.command("pc ⚠ Do NOT use Ranged/Magic damage on Kings // No Flaming Flay / No Term / No Crimson Stacks (Dominus) // Switch to Sorrow/Mythos armor and do the hit phase with melee only ⚠");
            });
         });
 
         Register.command("kingSpawn", args -> {
             Helper.sleep(200, () -> {
-                Chat.chat("Hi old man, give me your wool or I’m selling your brother’s soul to the devil—and yours too if I manage to grab it >:)");
+                Chat.command("pc Hi old man, give me your wool or I’m selling your brother’s soul to the devil—and yours too if I manage to grab it >:)");
             });
         });
 
         UseItemCallback.EVENT.register((player, world, hand) -> {
+            if (ffTimerOn) return ActionResult.PASS;
             ItemStack item = player.getMainHandStack();
             String itemName = ItemHelper.getItemName(item);
             if (itemName.contains("Fire Freeze Staff")) {
@@ -53,7 +54,7 @@ public class DianaFeatures {
         });
 
         Register.onTick(2, args -> {
-            if (endTime - startTime < 0) return;
+            if (endTime - startTime <= 0 || !ffTimerOn) return;
             startTime = System.currentTimeMillis();
             overlayText.text = "§a%.2fs".formatted(endTime - startTime);
         });
