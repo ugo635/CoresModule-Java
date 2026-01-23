f64da17d01a30ff8b8911c6c6d6854073a660d6a
ugo635 <ugolechien58901@gmail.com>
2026-01-23 22:28:19 +0100

    Works as intended allg and all fine

--- src/main/java/com/me/coresmodule/utils/render/gui/guis/ItemCustomizer/ItemCustomization.java
+++ src/main/java/com/me/coresmodule/utils/render/gui/guis/ItemCustomizer/ItemCustomization.java
@@ -29,19 +29,16 @@ public class ItemCustomization extends WindowScreen {
         UIComponent titleBox = new UIRoundedRectangle(7.5f)
                 .setX(new CenterConstraint())
                 .setY(new PixelConstraint(12.5f))
-                .setWidth(new PixelConstraint(260f))
+                .setWidth(new PixelConstraint(0.8f * main.getWidth()))
                 .setHeight(new PixelConstraint(20f))
-                .setColor(new Color(40, 40, 40, 255))
-                .setChildOf(main);
+                .setColor(new Color(40, 40, 40, 255));
 
-        UIText titleText = (UIText) new UIText("CoresModule Item Customization")
+        UIText titleText = (UIText) new UIText()
+                .setText("CoresModule Item Customization")
                 .setX(new CenterConstraint())
                 .setY(new CenterConstraint())
                 .setTextScale(new PixelConstraint(1.5f))
-                .setColor(new Color(145, 5, 145, 255))
-                .setChildOf(titleBox);
-
-        GUIs.addRoundedBorder(titleBox, 1.5f, new Color(80, 80, 80, 255));
+                .setColor(new Color(145, 5, 145, 255));
 
         // Inputs
         UIComponent inputBox = new UIRoundedRectangle(5f)
@@ -49,23 +46,20 @@ public class ItemCustomization extends WindowScreen {
                 .setY(new PixelConstraint(50f))
                 .setWidth(new PixelConstraint(200f))
                 .setHeight(new PixelConstraint(25f))
-                .setColor(new Color(40, 40, 40, 255))
-                .setChildOf(main);
+                .setColor(new Color(40, 40, 40, 255));
 
-        UITextInput input = (UITextInput) new UITextInput("Enter item name...")
-                .setX(new PixelConstraint(5f))
-                .setY(new CenterConstraint())
-                .setWidth(new PixelConstraint(190f))
+        UITextInput input = (UITextInput) new UITextInput()
+                .setX(new PixelConstraint(new RelativeConstraint().getValue() + 5f))
+                .setY(new PixelConstraint(7.5f))
+                .setWidth(new PixelConstraint(200f))
                 .setHeight(new PixelConstraint(20f))
-                .setColor(new Color(255, 255, 255, 255))
-                .setChildOf(inputBox);
+                .setColor(new Color(255, 255, 255, 255));
 
+        input.setPlaceholder("Enter item name...");
         input.onMouseClick((inp, ignored) -> {
             input.grabWindowFocus();
             return null;
         });
-
-        GUIs.addBorder(inputBox, 1.5f, Color.WHITE);
 
         // Item Texture Display
         UIComponent itemDisplayBox = new UIBlock()
@@ -73,15 +67,37 @@ public class ItemCustomization extends WindowScreen {
                 .setY(new PixelConstraint(100f))
                 .setWidth(new PixelConstraint(260f))
                 .setHeight(new PixelConstraint(260f))
-                .setColor(new Color(40, 40, 40, 255))
-                .setChildOf(main);
+                .setColor(new Color(40, 40, 40, 255));
 
-        // Initialize and add the animated component
-        AnimatedItemComponent itemDisplay = new AnimatedItemComponent();
-        itemDisplay.setX(new CenterConstraint())
+        AnimatedItemComponent itemDisplay = (AnimatedItemComponent) GUIs.createItemTextureComponent()
+                .setX(new CenterConstraint())
                 .setY(new CenterConstraint())
                 .setWidth(new PixelConstraint(256f))
-                .setHeight(new PixelConstraint(256f))
-                .setChildOf(itemDisplayBox);
+                .setHeight(new PixelConstraint(256f));
+
+        //main.onMouseEnterRunnable(() -> {
+        //
+        //});
+
+        // <div>
+
+        // <h1> Item Customization </h1>
+        main.addChild(titleBox);
+        titleBox.addChild(titleText);
+        //GUIs.addShadow(titleBox);
+        GUIs.addRoundedBorder(titleBox, 1.5f, new Color (80, 80, 80, 255));
+
+        // <div>
+        // <input type="text" placeholder="Enter item name">
+        main.addChild(inputBox);
+        inputBox.addChild(input);
+        GUIs.addBorder(inputBox, 1.5f, Color.WHITE);
+        // </div>
+
+        // <img src="item_texture.png">
+        main.addChild(itemDisplayBox);
+        itemDisplayBox.addChild(itemDisplay);
+
+        // </div>
     }
 }
